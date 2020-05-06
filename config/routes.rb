110c34ccd0f2411f19de8b19aa98d7c6b6356d93
Shaun Gordon <s.gordonwebdev@gmail.com>
2020-05-06 12:32:41 +0100

    initial catergories model and routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
 
 	root 'home#index'
 
+	namespace :admin do
+		get 'dashboard/index'
+		
+		resources :categories
+	end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
