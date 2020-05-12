ce3b623b8173bd45ce75b6c8e1af43e06ad5c945
Shaun Gordon <s.gordonwebdev@gmail.com>
2020-05-12 15:25:49 +0100

    validation touch ups

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,6 @@ class Category < ApplicationRecord
 	accepts_nested_attributes_for :products, allow_destroy: true, reject_if: :all_blank
 
 	validates :title, presence: true
-	validates :listing_order, presence: true
+	validates :listing_order, presence: true, uniqueness: true
 
 end
